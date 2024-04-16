8e9df9e0bfa83f809fbdcd1790735e0d603bb234
ViniciusSViana <visouzaviana@gmail.com>
2024-04-15 22:26:57 -0300

    criação da interface de botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
+import { Button } from './components/common/Button'
+import { ButtonVariant } from './components/common/Button/Button.constants'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
+    <>
+    <Button variant={ButtonVariant.Default}>
+      Tenho um Abrigo
+    </Button>
+
+    <Button variant={ButtonVariant.OutLined}>
+      Quero adotar
+    </Button>
+
+    {/* <button onClick={() => setCount((count) => count + 1)}>
+      children
     </button>
+    {count} */}
+    </>
   )
 }
---
+++ src/components/common/Button/Button.constants.ts
@@ -0,0 +1,6 @@
+export enum ButtonVariant {
+    Default = 'default',
+    Disabled = 'disabled',
+    OutLined = 'outLined',
+    Text = 'text',
+}
