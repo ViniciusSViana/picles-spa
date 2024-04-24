a3f7e51224756c2fb4a8efbd63142b426f45a183
ViniciusSViana <visouzaviana@gmail.com>
2024-04-23 22:21:28 -0300

    envio de formulario

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,11 +1,16 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel/Panel";
 
 
-export  function Shelter() {
+export function Shelter() {
     return (
         <>
             <Panel>
-            Painel de Abrigo
+                <form>
+                    <Input label='Nome' />
+                    <Button type="submit">Salvar Dados</Button>
+                </form>
 
             </Panel>
         </>
